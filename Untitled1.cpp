dca72a7f037ac190ab1f11fe67ed1a6c663fe183
Giovanni1926 <62810928+Giovanni1926@users.noreply.github.com>
2022-12-19 10:49:56 +0100

    prova

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int cane(){
+int i=6;
+/*if(i>7){
+    return 7;
+}else{
+    return 27;
+}*7
+*/
+
+
+return (i>7)*7+(i<7)*27;
+}
+
+
+
+
+int main(){
+	
+  printf("%d",cane());
+  
+	
+}
